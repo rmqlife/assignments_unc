d6f0441fc3783e09988b92b2b23cf1fee1cf3138
rmqlife <rmqlife@dhcp-v880-13-00813.1Xwireless.unc.edu>
2016-10-04 16:13:24 -0400

    finish

--- cv_jmf3/HoughTransform.m
+++ cv_jmf3/HoughTransform.m
@@ -1,1 +1,31 @@
+clc; clear; close all;
 % Hough Transform
+[x,y] = Line2(100);
+
+
+canvas = zeros(500,180);
+phis = [];
+vis_mat = [20;200];
+for i = 1:size(x,1)
+    for theta = 1:180
+        phi = x(i)*cos(pi*theta/180) + y(i)* sin(pi*theta/180);
+        phi = round([phi,1]*vis_mat); 
+        phis = [phis;phi];
+        canvas(phi,theta) = canvas(phi,theta) +1 ;
+    end
+end
+figure;
+imagesc(canvas);
+% find the max
+[~, ind] = max(canvas(:));
+[phi,theta] = ind2sub(size(canvas),ind);
+% transfer back
+phi = (phi - vis_mat(2))/vis_mat(1);
+% the line is phi = x * cos(theta) + y * sin(theta);
+% m = -cos(theta)/sin(theta) 
+% b = phi/sin(theta)
+B = [-cot(theta*pi/180);phi/sin(theta*pi/180)];
+
+figure;
+plot(x,y,'r.'); hold;
+plot(x,[x,ones(size(x))]*B,'b');
