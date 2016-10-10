030c85c7eb636bdd61beb80c3c4a65002dff98c1
rmqlife <rmqlife@dhcp-v880-0-01201.1Xwireless.unc.edu>
2016-10-10 15:32:37 -0400

    fix the hough transform to find multiple line

--- cv_jmf3/HoughTransform.m
+++ cv_jmf3/HoughTransform.m
@@ -1,4 +1,4 @@
-function B = houghTransform(x,y,show_fag)
+function B_mat = houghTransform(x,y,thresh,show_fag)
 canvas = zeros(500,180);
 phis = [];
 vis_mat = [10;200];
@@ -10,18 +10,30 @@ for i = 1:size(x,1)
         canvas(phi,theta) = canvas(phi,theta) +1 ;
     end
 end
-
-% find the max
-[~, ind] = max(canvas(:));
-[phi,theta] = ind2sub(size(canvas),ind);
-% transfer back
-phi = (phi - vis_mat(2))/vis_mat(1);
-% the line is phi = x * cos(theta) + y * sin(theta);
-% m = -cos(theta)/sin(theta) 
-% b = phi/sin(theta)
-B = [-cot(theta*pi/180);phi/sin(theta*pi/180)];
 if show_fag
     figure;
     imagesc(canvas);
-
 end
+
+B_mat = [];
+while 1
+    % find the max
+    [val,ind] = max(canvas(:));
+    if val<thresh
+        break;
+    end
+    [phi,theta] = ind2sub(size(canvas),ind);
+    for i = phi-5:phi+5
+        for j = theta-5:theta+5
+            canvas(i,j) = 0;
+        end
+    end
+    % transfer back
+    phi = (phi - vis_mat(2))./vis_mat(1);
+    % the line is phi = x * cos(theta) + y * sin(theta);
+    % m = -cos(theta)/sin(theta) 
+    % b = phi/sin(theta)
+    B = [-cot(theta*pi/180);phi./sin(theta*pi/180)];
+    B_mat = [B_mat,B];
+end
+
--- cv_jmf3/HoughTransform_demo.m
+++ cv_jmf3/HoughTransform_demo.m
@@ -1,7 +1,7 @@
 function HoughTransform_demo
 % line1
 [x,y] = Line1(100);
-B_h = houghTransform(x,y,0)
+B_h = HoughTransform(x,y,40,0)
 B_ls = leastSquare(x,y)
 figure;
 plot(x,y,'r.'); hold;
@@ -9,10 +9,14 @@ plot(x,[x,ones(size(x))]*B_h,'b');
 plot(x,[x,ones(size(x))]*B_ls,'g');
 
 % line2
-[x,y] = Line2(100);
-B_h = houghTransform(x,y,0)
+[x2,y2] = Line2(100);
+x = [x;x2];
+y = [y;y2];
+B_h = HoughTransform(x,y,40,1)
 B_ls = leastSquare(x,y)
 figure;
 plot(x,y,'r.'); hold;
-plot(x,[x,ones(size(x))]*B_h,'b');
+for i = 1:size(B_h,2)
+    plot(x,[x,ones(size(x))]*B_h(:,i),'b');
+end
 plot(x,[x,ones(size(x))]*B_ls,'g');
