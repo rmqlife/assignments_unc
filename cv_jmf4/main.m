72602df73fc99cb4fb5215c482d08964fc6a2d7e
Rmqlife <rmqlife@gmail.com>
2016-10-28 16:35:50 -0400

    stable version of ransac to find the homography matrix

--- cv_jmf4/main.m
+++ cv_jmf4/main.m
@@ -1,14 +1,9 @@
 clc; clear; close all;
 im1 = imread('wall\im1.pgm');
-im2 = imread('wall\im2.pgm');
+im2 = imread('wall\im3.pgm');
 % naive method for corner detection
 pt1 = naive_corner_nms(im1);
 pt2 = naive_corner_nms(im2);
-% show corners
-imshow([im1,im2]); hold on;
-%plot(pt1(:,2),pt1(:,1),'ro');
-%plot(size(im1,2)+pt2(:,2),pt2(:,1),'bo');
-
 %feature descriptor
 feat1 = feature_descriptor(im1,pt1,20,5);
 feat2 = feature_descriptor(im2,pt2,20,5);
@@ -27,15 +22,18 @@ if 0
 end
 
 % brute match
-match_set = zeros(size(pt1,1),2);
+match_points = zeros(size(pt1,1),4);
 for i = 1:size(feat1,1)
    [val,ind] = min(pdist2(feat1(i,:),feat2));
-   %pt1(i,:),pt2(ind,:)
-   plot(pt1(i,2),pt1(i,1),'ro');
-   plot(pt2(ind,2)+size(im1,2),pt2(ind,1),'bo');
-   plot([pt1(i,2),pt2(ind,2)+size(im1,2)],[pt1(i,1),pt2(ind,1)],'g-');
-   match_set(i,:) = [i,ind];
+   match_points(i,:)=[pt1(i,:),pt2(ind,:)];
 end
+
+% show_match_points(im1,im2,match_points);
+final_match_points = ransac_homography(match_points);
+show_match_points(im1,im2,final_match_points);
+
+
+
 
 % RANSAC to filter the match set
 % find the homography and tranformation
--- cv_jmf4/ransac_homography.m
+++ cv_jmf4/ransac_homography.m
@@ -1,41 +1,48 @@
-%ransac_homography
+function [final_match_points,H] = ransac_homography(match_points)
+% ransac_homography
 % choose 4 pairs of matches
-
 max_match_count = 0;
 max_H = zeros(3,3);
 for repeat_times = 1:200
-    inds = randi(size(match_set,1),[4,1]);
-    inds1 = match_set(inds,1);
-    inds2 = match_set(inds,2);
-    samp_pt1 = pt1(inds1,:);
-    samp_pt2 = pt2(inds2,:);
-
+    % sample at least 4 points to compute the homography
+    inds = randi(size(match_points,1),[4,1]);
+    samp_pts = match_points(inds,:);
+    % compute the homography with the sample points pairs
     A = [];
     for i=1:4
-        row = homography_mat(samp_pt1(i,1),samp_pt1(i,2),samp_pt2(i,1),samp_pt2(i,2));
+        row = homography_mat(samp_pts(i,1),samp_pts(i,2),samp_pts(i,3),samp_pts(i,4));
         A = [A;row];
     end
     % V:eigen vectors and D:eigenvalues
     [V,D] = eig(A'*A);
-    % smallest
+    % homography transformation matrix
     H = reshape(V(:,1),[3,3])';
-
+    
+    % validate the result with all the data
     match_count=0;
-    for i = 1:size(match_set,1)
-        i1 = match_set(i,1);
-        i2 = match_set(i,2);
-        pt1_project = (H*[pt1(i1,:),1]')';
+    for i = 1:size(match_points,1)
+        pt1_project = (H*[match_points(i,1:2),1]')';
         pt1_project = [pt1_project(1)/pt1_project(3),pt1_project(2)/pt1_project(3)]';
-        project_dist = pdist2(pt1_project',pt2(i2,:));
+        project_dist = pdist2(pt1_project',match_points(i,3:4));
         if project_dist<10
             match_count=match_count+1;
         end
     end
-    % project 
+    % record the max match result 
     if match_count>max_match_count
         max_H = H;
         max_match_count = match_count;
     end;
 end;
 
+final_match_points = [];
 % show the match result
+for i = 1:size(match_points,1)
+    pt1_project = (max_H*[match_points(i,1:2),1]')';
+    pt1_project = [pt1_project(1)/pt1_project(3),pt1_project(2)/pt1_project(3)]';
+    project_dist = pdist2(pt1_project',match_points(i,3:4));
+    if project_dist<10
+        final_match_points = [final_match_points;match_points(i,:)];
+    end
+end
+H = max_H;
