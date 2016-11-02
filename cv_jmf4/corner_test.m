b98a8b8e0b614d2855824ebe006faeb5a81e8b6e
rmqlife <rmqlife@rmqlifedeMacBook-Air.local>
2016-11-02 01:28:04 -0400

    update 1

--- cv_jmf4/corner_test.m
+++ cv_jmf4/corner_test.m
@@ -1,5 +1,12 @@
-im = imread('wall/im1.pgm');
-pt = naive_corner(im);
-imshow(im);
-hold on;
-plot(pt(:,2),pt(:,1),'ro');
+imset = {'im1.pgm','im2.pgm','im3.pgm','im4.pgm','im5.pgm','im6.pgm'};
+
+for i = 1:length(imset)
+    imset{i}
+    im = imread(['wall/',imset{i}]);
+    %pt = naive_corner(im);
+    pt = naive_corner_nms(im);
+    figure,imshow(im);
+    hold on;
+    plot(pt(:,2),pt(:,1),'ro');
+    hold off;
+end
